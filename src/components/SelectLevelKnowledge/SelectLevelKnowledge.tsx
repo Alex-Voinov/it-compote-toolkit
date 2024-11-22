78c5549afb8c79d728bb0f61ae578e53bf2266bf
Alex Frost <VoinovAlex1900@yandex.ru>
2024-11-22 23:35:45 +0300

    Реализован выбор уровень ученика

--- src/components/SelectLevelKnowledge/SelectLevelKnowledge.tsx
+++ src/components/SelectLevelKnowledge/SelectLevelKnowledge.tsx
@@ -13,6 +13,32 @@ const setButtonTexts = [
 const SelectLevelKnowledge: FC = () => {
     const { student } = useContext(GlobalData)
     const { FirstName, LastName, Disciplines } = student.student!
+    const buttons = []
+    let buffer = []
+    for (let level of setButtonTexts) {
+        buffer.push(
+            <button
+                onClick={
+                    e => {
+                        e.preventDefault();
+                        student.setLevel(level);
+                    }
+                }>
+                {level}
+            </button>
+        )
+        if (buffer.length === 2) {
+            buttons.push(<div>
+                {buffer}
+            </div>)
+            buffer = []
+        }
+    }
+    if (buffer) buttons.push(
+        <div style={{ justifyContent: 'center' }}>
+            {buffer}
+        </div>
+    )
     return (
         <section className={styles.wrapper}>
             <header>
@@ -20,20 +46,22 @@ const SelectLevelKnowledge: FC = () => {
                 <div>{`${FirstName} ${LastName}`}</div>
             </header>
             <main>
-                {Disciplines.map(discipline => <div>
-                    {discipline.Discipline}: {discipline.Level}
-                </div>)}
+                {Disciplines.length ? Disciplines.map(discipline => <div
+                    className={styles.disciplineRow}
+                >
+                    <div>
+                        {discipline.Discipline}
+                    </div>
+                    <div>
+                        {discipline.Level?discipline.Level:'Не указано'}
+                    </div>
+                </div>)
+                    : <div className={styles.notFound}>
+                        У студента не найдено ни одной активной или завершенной дисциплины.
+                    </div>}
             </main>
             <div className={styles.buttonBlock}>
-                {setButtonTexts.map(level => <button
-                    onClick={
-                        e => {
-                            e.preventDefault();
-                            student.setLevel(level);
-                        }
-                    }>
-                    {level}
-                </button>)}
+                {buttons}
             </div>
         </section>
     )
